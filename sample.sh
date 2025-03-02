4e1dd66be043e08fb46d2485a433e95041b9880c
Sachin D <sachin.d28@outlook.com>
2025-03-02 17:04:33 +0530

    updated sample.sh

--- sample.sh
+++ sample.sh
@@ -1,7 +1,10 @@
 hi...this is to check the file
 this is the second file to edit
 <<<<<<< HEAD
+<<<<<<< HEAD
 this is the 3rd file to edit
 =======
 this is my third file to edit
 >>>>>>> 0b83c911ceea9ef7fcf0f94fad8d5832e86d6c97
+=======
+>>>>>>> parent of 3d981ec (updated sample.sh)

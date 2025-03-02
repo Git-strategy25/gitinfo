1bfa09506f64deea6c10af0c409c78c5e02863cd
Sachin D <sachin.d28@outlook.com>
2025-03-02 17:51:13 +0530

    changes reverted:Revert "updated sample.sh"
    This reverts commit bd6d4273760ea7dd831b4c1752783b0db916b442.

---
+++ sample.sh
@@ -0,0 +1,25 @@
+hi...this is to check the file
+this is the second file to edit
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 4e1dd66 (updated sample.sh)
+=======
+>>>>>>> parent of 8a5fa8a (updated sample.sh)
+this is the 3rd file to edit
+=======
+this is my third file to edit
+>>>>>>> 0b83c911ceea9ef7fcf0f94fad8d5832e86d6c97
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 3d981ec (updated sample.sh)
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 3d981ec (updated sample.sh)
+=======
+>>>>>>> parent of 4e1dd66 (updated sample.sh)
+=======
+>>>>>>> parent of 8a5fa8a (updated sample.sh)
